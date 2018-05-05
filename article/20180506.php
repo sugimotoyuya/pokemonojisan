73beb5c401455c0af98f5f374c4fefe429348932
yuya-sugimoto <yuyataitoku7728@gmail.com>
2018-05-06 08:48:56 +0900

    初期コミット

---
+++ article/20180506.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width,initial-scale=1.0" name="viewport">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="../css/article.css">
+    <link rel="stylesheet" type="text/css" href="../css/index.css">
+    <title>ぽけおじの隠れ家</title>
+</head>
+<body>
+    
+    <div id="mainview">
+        <div class="h1"><a class="a1" href="../index.php">ぽけおじの隠れ家</a></div>
+    </div>
+
+    <div class="contents">
+
+        <div class="items">
+            <p class="pkmimg">
+                <img src="../img/20180506/randorosu.png" alt="ランドロス">
+            </p>
+            <div>
+                <p class="pkmname">ランドロス</p>
+                <p class="desc">
+
+                </p>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="footer">
+        <div class="footerIn">
+            <ul>
+                <li><a href="inquiry.php">お問合せ</a></li>
+                <li><a href="me.php">ぽけおじさんの紹介</a></li>
+            </ul>
+        </div>
+    </div>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width,initial-scale=1.0" name="viewport">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+    <title>ぽけおじの隠れ家</title>
+</head>
+<body>
+    
+    <div id="mainview">
+        <div class="h1">ぽけおじの隠れ家</div>
+    </div>
+
+    <div class="contents">
+
+        <div class="items">
+            <p class="date">2018年5月5日</p>
+            <p class="title"><a href="article/20180506.php">ポケモンプレゼント【5月6日〆】</a></p>
+            <p class="description">
+                今週のプレゼントポケモンは、PGLトップ3のポケモン<br>
+                ※個体値は、あなたが好きな個体値にすることが可能です。<br>
+                ※努力値は、配分されていません！
+            </p>
+        </div>
+
+    </div>
+
+    <div class="footer">
+        <div class="footerIn">
+            <ul>
+                <li><a href="inquiry.php">お問合せ</a></li>
+                <li><a href="me.php">ぽけおじさんの紹介</a></li>
+            </ul>
+        </div>
+    </div>
+
+</body>
+</html>
